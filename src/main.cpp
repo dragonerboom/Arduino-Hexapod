ab06036522b09022925475dc425ac26e84a64d2c
dragonerboom <antonopoulosv2@gmail.com>
2026-06-12 18:30:11 +0300

    Swaped sero library for PCA9685 compatible adafruit library, refactored leg class

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
-#include <Servo.h>
 #include <math.h>
 #include <Ramp.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
 // ########################################## PHYSICAL CONSTANTS
 const double L_COXA  = 57.0;  // J1L
@@ -10,40 +11,51 @@ const double L_TIBIA = 110.0; // J3L
 
 const double Y_REST = 80.0;   // Neutral distance from body
 const double Z_REST = -90.0;  // Standing height
-const double TIBIA_OFFSET_ANGLE = 0; 
+const int ServoMin = 500;
+const int ServoMax = 2000;
 
-// ########################################## UNIFIED LEG CLASS
+//##################################CLASSES########################
 class UnifiedLeg {
   private:
     struct Joint {
-      Servo servo;
-      bool inverted;
-      int8_t offset;
+      Adafruit_PWMServoDriver* driver; // Pointer to assigned driver board
+      uint8_t channel;                 // driver pin channel (0-15)
+      bool inverted;                   
+      int8_t offset;                   
+
       void update(double angle) {
-        double finalAngle = inverted ? (180 - angle + offset) : (angle + offset);
-        servo.write(constrain(finalAngle, 0, 180));
+        double finalAngle = inverted ? (180.0 - angle + offset) : (angle + offset);
+        finalAngle = constrain(finalAngle, 0.0, 180.0);
+
+        // Convert target angle directly to microsecond pulse limits
+        int pulseWidthMicros = map(finalAngle, 0.0, 180.0, ServoMin, ServoMax);
+        
+        driver->writeMicroseconds(channel, pulseWidthMicros);
       }
     };
 
     Joint j1, j2, j3;
-    rampDouble posX, posY, posZ;
+    rampDouble posX, posY, posZ; 
     double mountAngleRad;
 
   public:
-    void setup(uint8_t p1, bool i1, int8_t o1, 
-               uint8_t p2, bool i2, int8_t o2, 
-               uint8_t p3, bool i3, int8_t o3, 
+    void setup(Adafruit_PWMServoDriver &board, 
+               uint8_t ch1, bool i1, int8_t o1, 
+               uint8_t ch2, bool i2, int8_t o2, 
+               uint8_t ch3, bool i3, int8_t o3, 
                float mAngleDeg) {
       
-      j1.servo.attach(p1, 500, 2500); j1.inverted = i1; j1.offset = o1;
-      j2.servo.attach(p2, 500, 2500); j2.inverted = i2; j2.offset = o2;
-      j3.servo.attach(p3, 500, 2500); j3.inverted = i3; j3.offset = o3;
+      // Map physical channels and orientations
+      j1.driver = &board; j1.channel = ch1; j1.inverted = i1; j1.offset = o1;
+      j2.driver = &board; j2.channel = ch2; j2.inverted = i2; j2.offset = o2;
+      j3.driver = &board; j3.channel = ch3; j3.inverted = i3; j3.offset = o3;
       
       mountAngleRad = mAngleDeg * (PI / 180.0);
       
       posX.go(0);
       posY.go(0);
       posZ.go(0);
+      delay(100);
     }
 
     void setTarget(double tx, double ty, double tz, uint32_t duration) {
@@ -67,26 +79,33 @@ class UnifiedLeg {
       double z = curZ + Z_REST;
       y += Y_REST;
 
-      // 2. INVERSE KINEMATICS
-      double J1_A = atan2(x, y) * (180 / PI);
+      // 2. INVERSE KINEMATICS (Pure Geometric Solves)
+      double J1_A = atan2(x, y) * (180.0 / PI);
       double H = sqrt((y * y) + (x * x)) - L_COXA; 
       double L = sqrt((H * H) + (z * z));
       
-      // Law of Cosines for J3
-      double J3_A = acos(constrain(((L_FEMUR * L_FEMUR) + (L_TIBIA * L_TIBIA) - (L * L)) / (2 * L_FEMUR * L_TIBIA), -1, 1)) * (180 / PI);
+      // Law of Cosines for J3 (Tibia angle relative to Femur)
+      double J3_A = acos(constrain(((L_FEMUR * L_FEMUR) + (L_TIBIA * L_TIBIA) - (L * L)) / (2.0 * L_FEMUR * L_TIBIA), -1.0, 1.0)) * (180.0 / PI);
       
-      // Law of Cosines for J2
-      double B = acos(constrain(((L * L) + (L_FEMUR * L_FEMUR) - (L_TIBIA * L_TIBIA)) / (2 * L * L_FEMUR), -1, 1)) * (180 / PI);
-      double A = atan2(z, H) * (180 / PI);
+      // Law of Cosines for J2 (Femur angle relative to Horizon)
+      double B = acos(constrain(((L * L) + (L_FEMUR * L_FEMUR) - (L_TIBIA * L_TIBIA)) / (2.0 * L * L_FEMUR), -1.0, 1.0)) * (180.0 / PI);
+      double A = atan2(z, H) * (180.0 / PI);
       double J2_A = (B + A);
 
-      // 3. SERVO UPDATES
-      j1.update(90 - J1_A);
-      j2.update(90 - J2_A);
-      j3.update(j3.inverted ? (180 - (J3_A - TIBIA_OFFSET_ANGLE)) : (J3_A + TIBIA_OFFSET_ANGLE));
+      // 3. HARDWARE POSITIONING
+      // Joint 1 (Coxa): Centered at 90 deg, sweeps relative to Z-axis entry
+      j1.update(90.0 - J1_A);
+      
+      // Joint 2 (Femur): Centered at 90 deg relative to horizontal datum
+      j2.update(90.0 - J2_A);
+      
+      // Joint 3 (Tibia): Cleaned of spatial offsets, maps pure physical closing angle
+      j3.update(J3_A);
     }
 };
 
+Adafruit_PWMServoDriver boardLeft = Adafruit_PWMServoDriver(0x40);
+Adafruit_PWMServoDriver boardRight = Adafruit_PWMServoDriver(0x41);
 // ########################################## GAIT SETTINGS
 const double walkSteps[13][2] = {
     {0.0, 0.0}, 
@@ -114,15 +133,10 @@ double stickAngle = 0.0; // 0 = Forward, 180 = Backward
 double rotation = 0.0; 
 
 void setup() {
-  // PIN ASSIGNMENTS (Fixed Pin 1 and Duplicate Pin 16 issues)
-  // Format: (J1, Inv, Off, J2, Inv, Off, J3, Inv, Off, MountAngle)
-  legs[0].setup(22, false, 0, 23, true,  0, 24, false, 0, 60);  // Front Right
-  legs[1].setup(25, false, 0, 26, true,  0, 27, false, 0, 0);   // Mid Right
-  legs[2].setup(28, false, 0, 29, true,  0, 30, false, 0, 300); // Back Right
-  legs[3].setup(31, true,  0, 32, false, 0, 33, true,  0, 240); // Back Left
-  legs[4].setup(34, true,  0, 35, false, 0, 36, true,  0, 180); // Mid Left
-  legs[5].setup(37, true,  0, 38, false, 0, 39, true,  0, 120); // Front Left
-  
+  boardLeft.begin();
+  boardLeft.setPWMFreq(50);
+  boardRight.begin();
+  boardRight.setPWMFreq(50);
   delay(2000);
 }
 
